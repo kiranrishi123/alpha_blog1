4cdd6db7b675b2bba1e9e1a840fbf1564e17301e
Rishi <rishi@idexcel.com>
2024-04-15 17:18:49 +0530

    Created user table

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_15_110129) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

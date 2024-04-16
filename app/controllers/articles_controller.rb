6f71dacc422d849b855a1aa0e07b81523a4c1b88
Rishi <rishi@idexcel.com>
2024-04-16 11:39:59 +0530

    user validation is done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,18 +1,15 @@
-class ArticlesController < ApplicationController
+class ArticlesController <ApplicationController
 	def show
-
 		@article = Articles.find(params[:id])
-
-		
-
-
 	end
+
 	def index
 		@articles =Articles.all
 	end
 	def new 
 		@article =Articles.new
 	end
+
 	def create
 		@article = Articles.new(params.require(:article).permit(:articles, :descprition))
 		if @article.save
@@ -23,27 +20,25 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
+
 	def edit
 		@article = Articles.find(params[:id])
 
 	end
 
 	def update
-
 		@article =Articles.find(params[:id])
 		if @article.update(params.require(:articles).permit(:articles, :descprition))
 			flash[:notice] ="Your Article has updated sucesffully"
 			redirect_to @article
 		else
 			render 'show'
-		
 		end
 
 	end
 	def destroy
-
 		@article =Articles.find(params[:id])
 		@article.destroy
-		redirect_to article_path
+		redirect_to articles_path
 	end
 end
